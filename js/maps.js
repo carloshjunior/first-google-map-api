8cfcc04d96145eff3575f40c3541b8e4385f5f3d
Carlos Henrique F G Junior <carloshjunior@gmail.com>
2018-06-15 22:41:39 +1200

    Styling google Maps

--- js/maps.js
+++ js/maps.js
@@ -1,10 +1,71 @@
 var map;
 
 function initMap(){
+  var styles = [
+    {
+      "featureType": "administrative",
+      "elementType": "all",
+      "stylers": [{"saturation": "-100"}]
+    },
+    {
+      "featureType": "administrative.province",
+      "elementType": "all",
+      "stylers": [{"visibility": "off"}]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "all",
+      "stylers": [{"saturation": -100},{"lightness": 65},{"visibility": "on"}]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [{"saturation": -100},{"lightness": "50"},{"visibility": "simplified"}]
+    },
+    {
+      "featureType": "road",
+      "elementType": "all",
+      "stylers": [{"saturation": "-100"}]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "all",
+      "stylers": [{"visibility": "simplified"}]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "all",
+      "stylers": [{"lightness": "30"}]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "all",
+      "stylers": [{"lightness": "40"}]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [{"saturation": -100},{"visibility": "simplified"}]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [{"hue": "#ffff00"},{"lightness": -25},{"saturation": -97}]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels",
+      "stylers": [{"lightness": -25},{"saturation": -100}]
+    }
+  ];
+
   map = new google.maps.Map(document.getElementById('map'),{
     center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13
+    zoom: 13,
+    styles: styles,
+    mapTypeControl:false
   });
+
 
   var markers = [];
   // location
